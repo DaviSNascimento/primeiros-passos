94ebbd6db43873c7fb7f2fbdea68bf68e0f0a877
Davi S. Nascimento <100232646+DaviSNascimento@users.noreply.github.com>
2023-03-11 01:11:27 +0000

    testev1

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
-class Main {
+
+/**
+ * Main
+ */
+public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println("look this");
+    var name = "Davi";
+    System.out.println("Hello " + name + " how are you?");
+   
   }
 }
